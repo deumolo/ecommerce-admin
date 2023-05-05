0d3cc6948d9218be39edd21e75106d426a4f6d16
Daniel <deumolo@gmail.com>
2023-05-05 00:35:58 -0500

    Created individual pages for home, orders ,products, settings

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,19 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        <div className="bg-blue-900 h-screen w-screen flex items-center">
-          <div className="text-center w-full">
-            <button
-              className="bg-white rounded-lg p-2 px-4"
-              onClick={() => signOut()}
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-      </>
-    );
-  }
   return (
-    <>
-      <div className="bg-blue-900 h-screen w-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            className="bg-white rounded-lg p-2 px-4"
-            onClick={() => signIn("google")}
-          >
-            Login with google
-          </button>
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2 className="text-blue-900">
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex items-center bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <img className="w-6 h-6" src={session?.user?.image} alt="" />
+          <div className="px-2">{session?.user?.name}</div>
         </div>
       </div>
-    </>
+    </Layout>
   );
 }
